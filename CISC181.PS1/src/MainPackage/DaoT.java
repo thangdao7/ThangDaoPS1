fd6e0e365255316b78e16c2dda68d33f06515771
Thang Dao <thangdao@udel.edu>
2016-02-15 20:34:57 -0800

    Initial Commit

---
+++ CISC181.PS1/src/MainPackage/DaoT.java
@@ -0,0 +1,9 @@
+package MainPackage;
+
+public class DaoT {
+	public static void HelloWorld(){
+		
+		System.out.println("Hello World");
+	}
+
+}
---
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -0,0 +1,14 @@
+package MainPackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * My GitHub account is: thangdao7
+		 */
+		
+		DaoT.HelloWorld();
+	}
+
+}
